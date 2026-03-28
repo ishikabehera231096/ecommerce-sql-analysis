97a178e908b6193316d3ebb1da86a483cec1a74f
Ishika Behera <ishikabehera23@gmail.com>
2026-03-28 23:54:00 +0530

    Day 3: Customer behavior analysis - identified low retention and customer segmentation

---
+++ queries/03_customer_behavior.sql
@@ -0,0 +1,26 @@
+-- New vs Returning customers 
+
+WITH noforders AS (
+    SELECT customer_unique_id,COUNT(DISTINCT order_id) AS total_orders
+    FROM customers AS c
+    JOIN orders AS o
+    ON c.customer_id = o.customer_id
+    WHERE order_status = 'delivered'
+    GROUP BY customer_unique_id
+    ORDER BY total_orders DESC
+),
+
+typeofcustomer AS (
+    SELECT CASE WHEN total_orders > 1 THEN 'Returning'
+            WHEN total_orders = 1 THEN 'New'
+            ELSE NULL
+            END AS customer_type
+    FROM noforders
+)
+             
+
+SELECT customer_type,
+       COUNT(*) AS number_of_customers,
+       ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (), 2) AS percentage_of_customer_type
+FROM typeofcustomer 
+GROUP BY customer_type
